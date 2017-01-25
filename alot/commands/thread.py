ryneeverett/alot

commit 2c85e7322f7a060309ee83e5906c731d3d8f5da6

make addresses unique in To headers

alot/commands/thread.py
@@ -231,7 +231,20 @@ class ReplyCommand(Command):
                         my_addresses, mail.get_all('Cc', []))
                     envelope.add('Cc', decode_header(', '.join(cc)))
 
-        to = ', '.join(recipients)
+        def ensure_unique_address(recipients):
+            """
+            clean up a list of name,address pairs so that
+            no address appears multiple times.
+            """
+            res = dict()
+            for name, address in getaddresses(recipients):
+                res[address] = name
+            urecipients = ['"{}" <{}>'.format(n, a) if n != ''
+                           else a
+                           for a, n in res.iteritems()]
+            return sorted(urecipients)
+
+        to = ', '.join(ensure_unique_address(recipients))
         logging.debug('reply to: %s', to)
 
         if self.listreply:
@@ -295,7 +308,7 @@ class ReplyCommand(Command):
                     new_value.append('"%s" <%s>' % (name, address))
                 else:
                     new_value.append(address)
-        return ', '.join(new_value)
+        return new_value
 
 
 @registerCommand(MODE, 'forward', arguments=[

commit b19311109f108e5b030c4934426fa304e512953b

Fix bug when replying to all
The bug and fix were found by @josch.  One needs to set
`honor_followup_to = True` and `followup_to = True` to trigger the bug.

See #874 for more info.  The bug was probably introduced in
2c85e7322f7a060309ee83e5906c731d3d8f5da6.

alot/commands/thread.py
@@ -195,7 +195,7 @@ class ReplyCommand(Command):
         sender = mail['Reply-To'] or mail['From']
         my_addresses = settings.get_addresses()
         sender_address = parseaddr(sender)[1]
-        cc = ''
+        cc = []
 
         # check if reply is to self sent message
         if sender_address in my_addresses:
@@ -256,7 +256,7 @@ class ReplyCommand(Command):
         # set Mail-Followup-To header so that duplicates are avoided
         if settings.get('followup_to'):
             # to and cc are already cleared of our own address
-            allrecipients = [to] + [cc]
+            allrecipients = [to] + cc
             lists = settings.get('mailinglists')
             # check if any recipient address matches a known mailing list
             if any(addr in lists for n, addr in getaddresses(allrecipients)):